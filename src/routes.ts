160a11b8f13290e376242786c6a5b06c8c23b7ff
DanMarzo <marzogildan@gmail.com>
2023-11-14 19:59:37 -0300

    Incluindo novo controller e service

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Response, Request } from "express"
 import { CreateUserController } from "./controllers/user/CreateUserController"
 import { AuthUserController } from "./controllers/user/AuthUserController"
+import { DetailUserController } from "./controllers/user/DetailsUserController"
 
 const router = Router()
 
@@ -8,6 +9,8 @@ const router = Router()
 router.post("/users", new CreateUserController().handle)
 
 router.post("/session", new AuthUserController().handle)
+
+router.get("/me", new DetailUserController().handle)
 
 
 export{router}
