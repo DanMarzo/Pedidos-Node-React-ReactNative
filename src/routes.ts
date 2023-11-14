d3ffc4bf023917e1b604f2aafd86505fdf44b7c7
DanMarzo <marzogildan@gmail.com>
2023-11-14 20:40:15 -0300

    Criando Rotas e preparando para cadastro de categories

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController"
 import { AuthUserController } from "./controllers/user/AuthUserController"
 import { DetailUserController } from "./controllers/user/DetailsUserController"
 import { isAuthenticated } from "./middlewares/isAuthenticated"
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController"
 
 const router = Router()
 
@@ -12,6 +13,10 @@ router.post("/users", new CreateUserController().handle)
 router.post("/session", new AuthUserController().handle)
 
 router.get("/me", isAuthenticated, new DetailUserController().handle)
+
+//Rotas category
+
+router.post("/category", isAuthenticated, new CreateCategoryController().handle)
 
 
 export{router}
