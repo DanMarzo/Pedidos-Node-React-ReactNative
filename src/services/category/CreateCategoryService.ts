95c0578d002d036ad71e0c87b3659a320366516a
DanMarzo <marzogildan@gmail.com>
2023-11-14 21:33:12 -0300

    Service e controller para cadastro de categorias

--- src/services/category/CreateCategoryService.ts
+++ src/services/category/CreateCategoryService.ts
@@ -1,8 +1,19 @@
 import prismaClient from "../../prisma"
 
+interface CategoryRequest {
+    name: string;
+}
+
 class CreateCategoryService {
-    async execute() {
-        return{ok:true}
+    async execute({ name }: CategoryRequest) {
+        if (name === "") {
+            throw new Error("Name invalid");
+        }
+        const category = await prismaClient.category.create({
+            data: { name },
+            select: { name: true, id: true, create_at: true, update_at: true }
+        })
+        return category
     }
 }
-export{CreateCategoryService}
+export { CreateCategoryService }
