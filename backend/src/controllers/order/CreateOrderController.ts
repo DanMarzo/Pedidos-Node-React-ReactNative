c0285890f7a9cf306ccca0631941155bc3a99b5e
DanMarzo <marzogildan@gmail.com>
2023-11-15 11:16:27 -0300

    Criando Rotas de Orders

---
+++ backend/src/controllers/order/CreateOrderController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { CreateOrderService } from "../../services/order/CreateOrderService";
+
+class CreateOrderController {
+    async handle(req: Request, res: Response) {
+        const { table, name } = req.body;
+        const createOrderService = new CreateOrderService();
+        const order = createOrderService.execute({ name, table });
+
+        res.json(order)
+
+    }
+}
+export { CreateOrderController }
