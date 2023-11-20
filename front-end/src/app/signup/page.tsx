68bf478b14d0cca9f7135c575a33579552c711a3
DanMarzo <marzogildan@gmail.com>
2023-11-20 11:08:04 -0300

    Incluindo Tela de cadastro

---
+++ front-end/src/app/signup/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import GlobalStyles from "@/styles/GlobalStyles";
+import { Button, Form, Input, Layout } from "antd";
+import { ThemeProvider } from "styled-components";
+import light from "@/styles/light";
+import Image from "next/image";
+import Logo from "@/../public/logo.svg";
+import { InputPwd, InputStyled } from "@/components/Inputs/InputComp";
+import BtnWithLoading from "@/components/Btn/Btn";
+import { Ancora } from "@/components/Ancoras/Ancoras";
+import Link from "next/link";
+import { ContentStyled, MainAuth } from "../page.styled";
+const { Item } = Form;
+export default function Home() {
+  return (
+    <ThemeProvider theme={light}>
+      <Layout>
+        <GlobalStyles />
+        {/* <HeaderStyled>
+          <TitleStyled>Sujeito Pizza</TitleStyled>
+        </HeaderStyled> */}
+        <ContentStyled>
+          <MainAuth>
+            <Image src={Logo} alt="Imagem Sujeito Pizza" />
+            <Form onFinish={(e) => console.log(e)} layout="horizontal">
+              <Item
+                name={"email"}
+                rules={[{ required: true, message: "Campo obrigatório" }]}
+              >
+                <InputStyled />
+              </Item>
+              <Item
+                name={"password"}
+                rules={[{ required: true, message: "Campo obrigatório" }]}
+              >
+                <InputPwd />
+              </Item>
+              <BtnWithLoading loading={true} type="submit">
+                Enviar
+              </BtnWithLoading>
+            </Form>
+            <Ancora href={"/signup"}>Não possui uma conta? Cadastre-se</Ancora>
+          </MainAuth>
+        </ContentStyled>
+      </Layout>
+    </ThemeProvider>
+  );
+}
