668a83ae3928c4e79374a950fb2e66ff83a7266a
DanMarzo <marzogildan@gmail.com>
2023-11-26 22:30:24 -0300

    Criando estilo Global

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./features/app";
+import GlobalStyled from "./shared/styles/GlobalStyled";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <GlobalStyled />
   </React.StrictMode>
 );
