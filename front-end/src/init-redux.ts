0f52fde8b9972dd73bed5c5d3e22eb427818e981
DanMarzo <marzogildan@gmail.com>
2023-11-27 22:20:57 -0300

    Ignorando a regra de serializacao

--- front-end/src/init-redux.ts
+++ front-end/src/init-redux.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./features/reducers"
+
+
 export default () => {
+
     const store = configureStore({
         reducer,
+        middleware: (getDefaultMiddleware) => {
+            return getDefaultMiddleware({
+                serializableCheck: false
+            });
+        }
     });
     return store;
 }
