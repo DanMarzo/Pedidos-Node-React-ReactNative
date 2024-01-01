0a468bd17d04ef4d4949e5e4ff6e0d9787128e2f
DanMarzo <marzogildan@gmail.com>
2024-01-01 16:09:33 -0300

    Configurando Eslint e prettier

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,36 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 0,
+  },
 };
