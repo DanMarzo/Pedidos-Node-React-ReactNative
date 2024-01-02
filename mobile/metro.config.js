197c0635bcb0768552c9d18545fefa55d3b766bf
DanMarzo <marzogildan@gmail.com>
2024-01-01 21:36:15 -0300

    Configuracao

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,11 +1,11 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const fs = require('fs');
 const path = require('path');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const rnwPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+  path.resolve(require.resolve('react-native-windows/package.json'), '..')
 );
 
 /**
@@ -20,7 +20,7 @@ const config = {
     blockList: exclusionList([
       // This stops "react-native run-windows" from causing the metro server to crash if its already running
       new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`
       ),
       // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
       new RegExp(`${rnwPath}/build/.*`),
